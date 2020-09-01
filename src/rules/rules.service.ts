b649bdca88b60eb6babbb7d9d325a3e6a8c493c0
injkgz <injecti0n.kg@gmail.com>
2020-09-01 18:40:53 +0700

    added two types of rules

--- src/rules/rules.service.ts
+++ src/rules/rules.service.ts
@@ -3,6 +3,32 @@ const groupRules = {
     conditions: {
       any: [
         {
+          fact: 'group',
+          operator: 'equal',
+          value: 'injecti0n.kg@gmail.com',
+          path: '$.title',
+        }
+      ],
+      
+    },
+    onSuccess() {
+      console.log('on success called');
+    },
+    onFailure() {
+      console.log('on failure called');
+    },
+    event: {
+      type: 'message',
+      params: {
+        data: 'hello-world!',
+      },
+    },
+  };
+
+  const userRules = {
+    conditions: {
+      any: [
+        {
           fact: 'users',
           operator: 'equal',
           value: 'injecti0n.kg@gmail.com',
@@ -32,8 +58,8 @@ export class RulesService {
       if(type === 'group'){
         return JSON.stringify(groupRules);      
       }
-      if(type === 'account'){
-        return JSON.stringify(groupRules);
+      if(type === 'user'){
+        return JSON.stringify(userRules);
       }
       else {
         throw new NotFoundException(type);
